f508afdd15129f8be73871e259962883ee90f8bd
roger <rparedes@costra.ec>
2024-12-06 21:55:23 -0500

    ui updates

--- metalstore/README.md
+++ metalstore/README.md
@@ -1,10 +1,18 @@
-# Bookstore
+# The True Metal Store
+Welcome to the metal music store! An app built to sell albums and all things heavy metal.
+Mission: to provide the largest selection of metal albums, also merch, and more.
+
+# Developed by Roger Paredes
+
+
+# Frameworks
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.12.
 
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+
 
 ## Code scaffolding
 
--- metalstore/src/app/components/reviews/reviews.component.ts
+++ metalstore/src/app/components/reviews/reviews.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ReviewsService, IReview } from '../../services/reviews.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css'
 })
 export class ReviewsComponent implements OnInit {
   reviews: IReview[] = [];
+  p: number = 1; // Current page number
+  itemsPerPage: number = 9; // Number of items per page
 
   constructor(private reviewService: ReviewsService) { }
 
